ad6a20a513172555edd14702a9e5b0579b9ab948
Jaakko Peurasaari <jakepsaari@gmail.com>
2025-12-09 17:37:16 +0200

    Gradle stuff

--- capacitor-camera/capacitor.config.ts
+++ capacitor-camera/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.capacitor.camera',
   appName: 'capacitor-camera',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: 'http://192.168.1.131:4200',
+    cleartext: true
+  }
 };
 
 export default config;
