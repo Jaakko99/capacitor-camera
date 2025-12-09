cf795c0906b7bd3aaf8aecf6959fa954369757cc
Jaakko Peurasaari <jakepsaari@gmail.com>
2025-12-09 15:45:12 +0200

    Add capacitor and plugins for it and delete placeholder html

---
+++ capacitor-camera/android/app/src/main/java/com/capacitor/camera/MainActivity.java
@@ -0,0 +1,5 @@
+package com.capacitor.camera;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor-camera/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.capacitor.camera',
+  appName: 'capacitor-camera',
+  webDir: 'dist'
+};
+
+export default config;
